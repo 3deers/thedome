0728fa851936552f0bf5a43bce4d213c5c2b8b31
Samuel <samjavaloyes96@gmail.com>
2020-01-14 21:31:27 +0100

    Made a f****** step

--- src/Star.js
+++ src/Star.js
@@ -1,33 +1,65 @@
 
 export default class Star {
     constructor(az, alt,dist , scene, geometry, material) {
+        dist=500
         this._coord = { az, alt }
+        this._dist = dist
+        this._position = new THREE.Vector3()
         this._geometry = geometry;
         this._material = material;
-        dist = 500
+        
         this._mesh = new THREE.Mesh(this._geometry, this._material);
         this._mesh.position.set( Math.cos(alt)*Math.cos(az)*(dist), Math.cos(alt)*Math.sin(az)*(dist),Math.sin(alt)*(dist) )
-       
+      
         scene.add(this._mesh)
+       
+        
     }
     getCoord (){
         return this._coord
     }
 
-    getMeshPos (){
+    getMeshPosition (){
         return this._mesh.position
     }
-    update(coord,dist){
+    getPosition (){
+        return this._position
+    }
+
+    setCoord(c){
+        this._coord=c
+    }
+    setDistance(d){
+        d=500
+        this._dist=d
+    }
+    getDistance(d){
+       return this._dist
+    }
+    setPosition(){
+        this._position.x=Math.cos(this._coord.alt)*Math.cos(this._coord.az)*(this._dist);
+        this._position.y=Math.cos(this._coord.alt)*Math.sin(this._coord.az)*(this._dist);
+        this._position.z=Math.sin(this._coord.alt)*(this._dist);
+    }
+    
+    update(coord){
+        
+      
         this._coord = coord
-        dist=500
-        this._mesh.position.set( Math.cos(coord.alt)*Math.cos(coord.az)*(dist), Math.cos(coord.alt)*Math.sin(coord.az)*(dist),Math.sin(coord.alt)*(dist) )
+        
+        
+        this._mesh.position.set( Math.cos(coord.alt)*Math.cos(coord.az)*(this._dist), Math.cos(coord.alt)*Math.sin(coord.az)*(this._dist),Math.sin(coord.alt)*(this._dist) )
         this._mesh.rotation.x += 0.01;
         this._mesh.rotation.y += 0.01;
         
-    //console.log(this._mesh.position)
+       
+
+    }
+    hide() {
+        this._mesh.visible = false;
     }
     render() {
-
+        this._mesh.visible = true;
     }
 
 }
--- src/index.js
+++ src/index.js
@@ -6,122 +6,166 @@ import Star from './Star.js';
 
 console.log(window.innerWidth)
 
-let open, renderer, scene, camera, light, tick=false, data,
+let open, renderer, scene, camera, light, tick = false, data,
 
-// starGeo = new THREE.SphereGeometry(1, 1, 1),
-starGeo  = new THREE.SphereBufferGeometry(1,1,1),
-starMat = new THREE.MeshLambertMaterial({ color: 'red' });
+  // starGeo = new THREE.SphereGeometry(1, 1, 1),
+  starGeo = new THREE.SphereBufferGeometry(1, 1, 1),
+  starMat = new THREE.MeshLambertMaterial({ color: 'red' });
+
+
+var numParticles = 10000;
+var positions = new Float32Array(numParticles * 3);
+var scales = new Float32Array(numParticles);
 
 let stars = []
-let date = new Date(2020,0,1,12,0,0);
+let date = new Date(2020, 0, 1, 12, 0, 0);
 
-function loadJSON(callback) {   
-    var xobj = new XMLHttpRequest();
-    xobj.overrideMimeType("application/json");
-    xobj.open('GET', '/data/skymap.json', true);
-    xobj.onreadystatechange = function () {
-      if (xobj.readyState == 4 && xobj.status == "200") {
-        callback(JSON.parse(xobj.responseText));
-      }
-    };
-    xobj.send(null);  
-  }
+function loadJSON(callback) {
+  var xobj = new XMLHttpRequest();
+  xobj.overrideMimeType("application/json");
+  xobj.open('GET', '/data/skymap.json', false);
+  xobj.onreadystatechange = function () {
+    if (xobj.readyState == 4 && xobj.status == "200") {
+      callback(JSON.parse(xobj.responseText));
+    }
+  };
+  xobj.send(null);
+}
+
 
 
 function init() {
 
- 
+
+
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  light = new THREE.PointLight(0xFFFFFF, 1, 500)
+  tick = false
+
+
+  camera.position.x = 0
+  camera.position.y = 0
+  camera.position.z = 0
+  renderer.setClearColor("#fcfcfc");
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  light.position.set(10, 0, 25)
+  camera.updateProjectionMatrix();
+  scene.add(light)
+  loadJSON(function (json) {
+    data = json
+    let star, coord
+    coord = AstrometryHelper.radec2azel(data[0].ra, data[0].dec, 0, 0, date)
+    var starRef = new Star(0, 0, 0, scene, starGeo, starMat)
+    for (let index = 0; index < data.length; index++) {
+
+      starRef = new Star(0, 0, 0, scene, starGeo, starMat)
+      coord = AstrometryHelper.radec2azel(data[index].ra, data[index].dec, 0, 0, date)
+      starRef.setCoord(coord)
+      starRef.setDistance(data[index].dist)
+      starRef.setPosition()
+      star = starRef
+      stars.push(star)
+    }
+    console.log(stars.length);
+
+  });
+
+  console.log(stars.length + "oh shit")
+  console.log(data.length)
+  for (var i = 0; i < 60000; i++) {
     
-    renderer = new THREE.WebGLRenderer({ antialias: true });
-    scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    light = new THREE.PointLight(0xFFFFFF, 1, 500)
-    tick = false
+    positions[i] = stars[i].getPosition().x;
+    positions[i + 1] = stars[i].getPosition().y;
+    positions[i + 2] = stars[i].getPosition().z;
+
+  }
+  console.log(positions[0])
+  console.log(positions[1])
+  console.log(positions[2])
+  console.log(stars[0].getDistance())
+  console.log(stars[1].getDistance())
+  console.log(positions[2])
+  var starsGeometry = new THREE.BufferGeometry();
+  starsGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
+  var starsMaterial = new THREE.PointsMaterial({ color: 'red' });
+  var starField = new THREE.Points(starsGeometry, starsMaterial);
+  scene.add(starField);
 
 
-    camera.position.x =0
-    camera.position.y =0
-    camera.position.z =0
-    renderer.setClearColor("#fcfcfc");
+
+
+  document.body.appendChild(renderer.domElement);
+  window.addEventListener('resize', () => {
+
     renderer.setSize(window.innerWidth, window.innerHeight);
-    light.position.set(10, 0, 25)
-    camera.updateProjectionMatrix();
-    scene.add(light)
-     loadJSON(function(json) {
-      data = json;
-      let num=20000,max=60000,star,coord
-        for (let index = 0; index < max; index+=(max/num)) {   
-            coord = AstrometryHelper.radec2azel(data[index].ra, data[index].dec, 0, 0,date)
-            star= new Star (coord.az,coord.alt,data[index].dist,scene, starGeo, starMat)
-            stars.push(star)
-        }
-    });
+    camera.aspect = window.innerWidth / window.innerHeight;
+  });
 
-   
-    document.body.appendChild(renderer.domElement);
-    window.addEventListener('resize', () => {
-   
-        renderer.setSize(window.innerWidth, window.innerHeight);
-        camera.aspect = window.innerWidth / window.innerHeight;
-    });
+  loop()
 
-    loop()
-
-    open = true
+  open = true
 }
 
 function update() {
-          for (let index = 0; index < stars.length; index++) {
-                let coord = AstrometryHelper.radec2azel(data[index].ra, data[index].dec, 0, 0,date)
-                stars[index].update(coord,data[index].dist)
-          }
-    //camera.rotation.y += (Math.PI / 180) * 0.2
-    // camera.rotation.z += (Math.PI / 180) * 0.2
-    //camera.position.z -= 10
-     date.setMinutes(date.getMinutes()+1);
-   
-    
-    
-    
+  let coord;
+  for (let index = 0; index < stars.length; index++) {
+    if(stars[index].getPosition().distanceTo(camera.position)<50){
+     coord = AstrometryHelper.radec2azel(data[index].ra, data[index].dec, 0, 0,date)
+     stars[index].update(coord,data[index].dist)
+      stars[index].render()
+  }else{
+    stars[index].hide()
+  }
+  }
+  // camera.rotation.y += (Math.PI / 180) * 0.2
+  // camera.rotation.z += (Math.PI / 180) * 0.2
+  camera.position.z += 5
+  // date.setMinutes(date.getMinutes()+1);
+
+
+
+
 }
 
 function allowUpdate() {
-tick = true
-console.log("allow updated");
+  tick = true
+  console.log("allow updated");
 }
 
 function render() {
-renderer.render(scene, camera);
+  renderer.render(scene, camera);
 }
 
 function loop() {
-    
+
   // tick=true;
-        //Procesamos el input
-        //TODO
-        //Si es el momento de updatear, updateamos
-        //ELAPSED_TIME > UPDATE_TICK_TIME
-        if (tick) {
-   
-          update();
-          tick=false
-          //console.log("updated");
-          
-          
-        }
-        render()
+  //Procesamos el input
+  //TODO
+  //Si es el momento de updatear, updateamos
+  //ELAPSED_TIME > UPDATE_TICK_TIME
+  if (tick) {
+
+    update();
+    tick = false
+    //console.log("updated");
 
 
-        requestAnimationFrame(loop)
-    
-    return true;
+  }
+  render()
+
+
+  requestAnimationFrame(loop)
+
+  return true;
 
 }
 
 
 
 
-setInterval(allowUpdate,33); // 33 milliseconds = ~ 30 frames per sec
+setInterval(allowUpdate, 33); // 33 milliseconds = ~ 30 frames per sec
 console.log("Oh")
 window.onload = init();
 
