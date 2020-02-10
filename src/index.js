c41d98b0a550108664d04a97ae28290a66adf40c
Esther <eng22@alu.ua.es>
2020-02-10 16:30:03 +0100

    Pick3D versión básica

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@
 //import * as GSAP from '../node_modules/gsap/dist/gsap.min.js';
 import AstrometryHelper from './AstrometryHelper.js';
 import Star from './Star.js';
-
+import Pick3D from './Pick3D.js';
 
 console.log(window.innerWidth)
 /**MAIN SCENE */
-let renderer, scene, camera, light, tick;
+let renderer, scene, pick3D, camera, light, tick;
 let date = new Date(2020, 0, 1, 12, 0, 0);
 
 /**STARS */
@@ -36,9 +36,10 @@ function loadJSON(callback) {
 
 function init() {
 
-
   /**LinitScene*/
   scene = new THREE.Scene();
+
+  pick3D = new Pick3D();
 
   renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setClearColor("#fcfcfc");
@@ -47,7 +48,7 @@ function init() {
   camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
   camera.position.x = 0;
   camera.position.y = 0;
-  camera.position.z = 0;
+  camera.position.z = 1000;
   camera.updateProjectionMatrix();
 
   light = new THREE.PointLight(0xFFFFFF, 1, 500)
@@ -93,6 +94,14 @@ function init() {
 
 
   document.body.appendChild(renderer.domElement);
+
+  window.addEventListener('mousedown', function (event) {
+    pick3D.setPickPosition(event);
+    pick3D.pick(scene.children, camera);
+  });
+
+  window.addEventListener('mouseup', pick3D.clearPickPosition());
+
   window.addEventListener('resize', () => {
 
     renderer.setSize(window.innerWidth, window.innerHeight);
@@ -149,17 +158,16 @@ function loop() {
   //TODO
   //Si es el momento de updatear, updateamos
   //ELAPSED_TIME > UPDATE_TICK_TIME
+
+
   if (tick) {
 
     update();
     tick = false
-    //console.log("updated");
 
 
   }
   render()
-
-
   requestAnimationFrame(loop)
 
   return true;
@@ -169,7 +177,7 @@ function loop() {
 
 
 
-setInterval(allowUpdate, 33); // 33 milliseconds = ~ 30 frames per sec
+setInterval(allowUpdate, 120); // 33 milliseconds = ~ 30 frames per sec
 console.log("Oh")
 window.onload = init();
 
